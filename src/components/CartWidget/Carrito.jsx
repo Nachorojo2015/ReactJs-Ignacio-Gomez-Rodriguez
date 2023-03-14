86a326174ad74b2b468acc276ae3988dee8ff2da
Ignacio Gomez <Nachorojo2015@gmail.com>
2023-03-14 18:38:51 -0300

    Cambios en el proyecto

--- src/components/CartWidget/Carrito.jsx
+++ src/components/CartWidget/Carrito.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './Carrito.css'
 
 const carrito = () => {
   return (
-    <a href=""><span class="material-symbols-outlined">
+    <a href=""><span className="material-symbols-outlined" id='carro'>
                           shopping_cart
                       </span><span>0</span></a>
   )
